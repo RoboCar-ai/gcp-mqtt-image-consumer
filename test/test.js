fa78993d5dbc0fc0cc8ac02a4aa38e0721033184
Thomas Milas <itpro41994@gmail.com>
2018-07-28 07:20:11 -0700

    filxed session download call

--- test/test.js
+++ test/test.js
@@ -10,7 +10,7 @@ file.download()
 .then(data => {
     const sess = JSON.parse(data.toString('utf8'));
     console.log(sess);
-    sess.status = 'active'
-    return file.save(JSON.stringify(sess));
+    // sess.status = 'active'
+    // return file.save(JSON.stringify(sess));
 })
 .catch(console.log);
